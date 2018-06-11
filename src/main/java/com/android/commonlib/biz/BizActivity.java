c950f06fb2bb6f5e26d5f5f59da7c4e5549dd88a
huangshunbo <737649308@qq.com>
2018-06-11 17:33:25 +0800

    init

---
+++ src/main/java/com/android/commonlib/biz/BizActivity.java
@@ -0,0 +1,13 @@
+package com.android.commonlib.biz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class BizActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biz);
+    }
+}
